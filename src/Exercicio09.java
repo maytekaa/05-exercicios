46bc11d42b0f4b81c2aa6fa4175d75c5c9e02e1e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 20:45:43 -0300

    Exercicios 08 e 09 finalizados

---
+++ src/Exercicio09.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercicio09 {
+
+    public static void main(String[] args) {
+
+        //entrada de dados
+        Scanner sc = new Scanner(System.in);
+        int valor, valorInvertido, unidade, dezena, centena;
+
+        //entrada de dados
+        System.out.print("Informe o valor com 3 digitos (100 = 999): ");
+        valor = sc.nextInt();
+
+
+        //processamento de dados
+        unidade = valor % 10;
+        dezena = valor % 100 /10;
+        centena = valor /100;
+
+        //processamento correto
+        valorInvertido = unidade * 100 + dezena * 10 + centena;
+        //processamento errado
+        System.out.print(unidade + "" + dezena + "" + centena);
+
+        //saida de dados
+        System.out.print("O novo valor é: "+ valorInvertido);
+    }
+}
